038e8dea95b00814eeb7740273e7736da554062a
AceHeYump <ars01@hotmail.com>
2016-01-21 14:30:03 -0700

    Added pullgif.go

---
+++ pullgif.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	//res, err := http.Get("http://www.mcleods.com/")
+	res, err := http.Get("http://stockcharts.com/def/servlet/SharpChartv05.ServletDriver?c=%24SILVER,PLTCDANRBO[PA][D][F1!3!!!4!20]&r=9926&pnf=y")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	page, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ioutil.WriteFile("c:\\temp\\silver.gif", page, 644)
+	res.Body.Close()
+
+	//fmt.Printf("%s", page)
+}
